0dbcb3e5046a70b86e573e7be9f1bbb5ccb1daa8
“Javier <you@gmail.com>
2020-06-22 21:36:18 -0500

    I am so done for tonight's practice

--- solutions.js
+++ solutions.js
@@ -20,3 +20,78 @@
  * and
  * concat
  */
+////////////////////////////////////////
+//  Define a function named isTrue that takes in any input
+//  and returns true if the input provided is exactly equal
+//  to true in value and data type.
+function isTrue(param) {
+return param === true;
+}
+
+//////////////////////////////////////////
+// Define a function named isFalse that takes in a value and
+// returns a true if and only if the provided input is equal
+// to false in both type and value.
+const isFalse = function (param) {
+    return param === false;
+}
+//////////////////////////////////////////
+// Define a function named not that takes in any input and
+// returns the boolean opposite of the provided input.
+function not(param) {
+return !param;
+}
+
+//////////////////////////////////////////
+// Define a function named addOne that takes in a single input.
+// If the input is a number or a numeric string,
+// return the value plus one.
+function addOne(param) {
+    if (!isNaN(param)) {
+        return parseFloat(param) + 1;
+    } else {
+            return NaN;
+        }
+    return parseFloat(param);
+    }
+
+///////////////////////////////////////////
+// Define a function named isEven that takes in a single input.
+// If the input is an even number or a string containing an even number,
+// return true. Any other input should return false for the output.
+function isEven(param) {
+ return parseFloat(param) % 2 === 0;
+}
+///////////////////////////////////////////
+// Define a function named isEqual that takes in two input arguments.
+// If each argument is equal only in value, then return true. Otherwise return false.
+function isEqual(param1, param2) {
+    return param1 == param2;
+}
+///////////////////////////////////////////
+// Define a function named or that takes in two input arguments. The output
+// returned should be the result of an or operation on both inputs.
+function or(param1, param2) {
+return param1 || param2;
+}
+///////////////////////////////////////////
+//Define a function named and that takes in two input arguments and returns
+// the result of a logical and operation of both inputs.
+function and(param1, param2) {
+return param1 && param2;
+}
+///////////////////////////////////////////
+// Define a function named concat that takes in two input arguments.
+// If both arguments are strings, then return the concatenated result.
+// If two numbers are provided, then return the string concatenation of
+// each set of numerals.
+function concat(param1, param2) {
+    return "" + param1 + param2;
+}
+//////////////////////////////////////////////
+//Define a function named isIdentical that takes in two input arguments.
+// If each input is equal to both in data type and in value, then return true.
+// If the values are not the same data type or not the same value, return false.
+function isIdentical(param1, param2) {
+    return param1 === param2;
+}
